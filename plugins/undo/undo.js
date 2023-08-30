b80801cb8ecdd52e7af22c74aa3dbe8611349c44
fledith <barateon023@gmail.com>
2023-08-30 19:58:30 +0300

    dark theme

--- plugins/undo/undo.js
+++ plugins/undo/undo.js
@@ -3,7 +3,7 @@
     start: function () {
       this.app.toolbar.add("undo", {
         title: "Undo",
-        icon: '<img style="filter: brightness(0)" class="icon icon-sm" src="https://peoplevine.blob.core.windows.net/media/361/36122e56ba5-7f62-48c8-96c1-096d59a7147a.png" />',
+        icon: '<img class="icon icon-sm" src="../../icons/undo.svg" alt="" />',
         command: "undo.toggle",
       });
     },
