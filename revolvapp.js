9c31569c2db787d278aa5c6ab75b27b089b8856e
fledith <barateon023@gmail.com>
2023-10-31 15:42:39 +0200

    order confirmation hapi code

--- revolvapp.js
+++ revolvapp.js
@@ -13752,6 +13752,16 @@
       };
       this.data = {
         class: { target: ["element"] },
+        "data-pv-component": { target: ["element"] },
+        "data-pv-type": { target: ["element"] },
+        "data-pv-parameters": { target: ["element"] },
+        "data-pv-object": { target: ["element"] },
+        "data-pv-element": { target: ["element"] },
+        "data-pv-repeater": { target: ["element"] },
+        "data-pv-format": { target: ["element"] },
+        "data-pv-formatter": { target: ["element"] },
+        "data-pv-do": { target: ["element"] },
+        "data-pv-conditional": { target: ["element"] },
       };
     },
     render: function () {
